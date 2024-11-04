5c00874a77ed5ff6643c57259e3fa3a659f70d40
Dag Sorensen <dag.sorensen@gmail.com>
2024-11-04 09:42:27 +0100

    app p1

--- ADI.App/Program.cs
+++ ADI.App/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Text.Json;
+using ADI.Services;
+using ADI.Services.Models;
+
+var openLibraryApiClient = new OpenLibraryApiClient();
+
+var searchQuery = new SearchQuery
+{
+    Author = "Brandon Sanderson",
+};
+
+Console.WriteLine(JsonSerializer.Serialize(await openLibraryApiClient.Search(searchQuery)));
